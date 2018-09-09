37ff936708c6e0c889a3822702d235e078979ec3
rich <richard.wota@gmail.com>
2018-09-10 00:58:48 +0900

    refactor TargetingSetSplitter#split in functional style

--- targeting_set_splitter.rb
+++ targeting_set_splitter.rb
@@ -2,27 +2,15 @@ require_relative 'target_set'
 
 class TargetingSetSplitter
   def split(target)
-    return split_dimension(TargetSet.new, target, [:countries, :placements, :gender_expanded, :age_range_expanded]).to_a
-  end
-
-  def split_dimension(template, target, dimensions)
-    sets = Set[]
-    dimension = dimensions.pop
-
-    target.send(dimension).each do |val| 
-      clone = template.clone
-      if dimension.to_s.index('_expanded').nil?
-        clone.send("#{dimension}=", [val])
-      else
-        clone.send("#{dimension.to_s.sub('_expanded', '')}=", val)
-      end
-
-      if dimensions.size == 0
-        sets.add(clone)
-      else
-        sets.merge(split_dimension(clone, target, dimensions.clone))
+    sets = target.countries.collect do |c| 
+      target.placements.collect do |p|
+        target.gender_expanded.collect do |g|
+          target.age_range_expanded.collect do |a|
+            TargetSet.new([c],[p],g,a)
+          end
+        end
       end
     end
-    return sets
+    return sets.flatten
   end
 end
